9cd658668d5f0861ef4b5699ccaf4e204f4e6417
calix-star <77974927+calix-star@users.noreply.github.com>
2021-01-27 10:57:55 +0900

    Add files via upload

---
+++ login.py
@@ -0,0 +1,31 @@
+import discord
+import time
+import configparser
+import sys
+
+config = configparser.ConfigParser()
+config.read('설정.ini')
+
+token = config['setting']['TOKEN']
+
+client = discord.Client()
+
+succes_password = '123'
+
+password = input("비밀번호를 입력해주세요\n")
+
+@client.event
+async def on_ready():
+    print(f'='*15+'로그인이 성공적으로 되었습니다.'+f'=' * 15)
+if password == str(succes_password):
+    print('어서오세요 Administrator 님')
+elif not password == str(succes_password):
+    print('비밀번호가 틀렸습니다.\n2초후 시스템이 종료됩니다.')
+    time.sleep(2)
+    sys.exit()
+else:
+    print('이상 접근이 감지되었습니다.\n2초후 시스템이 종료됩니다.')
+    time.sleep(2)
+    sys.exit()
+
+client.run(token)
